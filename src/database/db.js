7373235e975a9b34d4cac35aaec563d6a0f800c2
mariana rabelo <marianarabelo@cpejr.com.br>
2022-06-24 17:03:05 -0300

    correção

--- src/database/db.js
+++ src/database/db.js
@@ -2,52 +2,71 @@ const sqlite3 = require("sqlite3").verbose()
 
 const db = new sqlite3.Database("./src/database/database.db")
 
-db.serialize(() => {
+module.exports = db
+// db.serialize(() => {
 
-db.run(`
-    CREATE TABLE IF NOT EXISTS places (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        image TEXT,
-        name TEXT,
-        address TEXT,
-        address2 TEXT,
-        state TEXT,
-        city TEXT,
-        items TEXT
-    );
-        `)
+// db.run(`
+//     CREATE TABLE IF NOT EXISTS places (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         image TEXT,
+//         name TEXT,
+//         address TEXT,
+//         address2 TEXT,
+//         state TEXT,
+//         city TEXT,
+//         items TEXT
+//     );
+//         `)
 
-const query = `
-    INSERT INTO places (
-        image,
-        name,
-        address,
-        address2,
-        state,
-        city,
-        items
-    ) VALUES (?,?,?,?,?,?,?);
-    `
+// const query = `
+//     INSERT INTO places (
+//         image,
+//         name,
+//         address,
+//         address2,
+//         state,
+//         city,
+//         items
+//     ) VALUES (?,?,?,?,?,?,?);
+//     `
 
-const values = [
-    "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    "Colectoria",
-    "Guilherme Gemballa, Jardim América",
-    "Número 260",
-    "Santa Catarina",
-    "Rio do Sul",
-    "Resíduos Eletrônicos, Lâmpadas"
-]    
+// const values = [
+//     "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=929&q=80",
+//     "Papersider",
+//     "Guilherme Gemballa, Jardim América",
+//     "Número 260",
+//     "Santa Catarina",
+//     "Rio do Sul",
+//     "Papéis e Papelão"
+// ]    
 
-function afterInsertData(err) {
-    if(err) {
-        return console.log(err)
-    }
+// function afterInsertData(err) {
+//     if(err) {
+//         return console.log(err)
+//     }
 
-    console.log("Cadastrado com sucesso")
-    console.log(this)
-}
+//     console.log("Cadastrado com sucesso")
+//     console.log(this)
+// }
 
-db.run(query, values, afterInsertData)
+// db.run(query, values, afterInsertData)
 
-})
+
+// // db.all(`SELECT * FROM places`, function(err, rows){
+// //     if(err) {
+// //         return console.log(err)
+// //     }
+    
+// //     console.log("Aqui estão seus registros")
+// //     console.log(rows)
+// // })
+
+// db.run(`DELETE FROM places WHERE id = ?`, [7], function(err) {
+//     if(err) {
+//         return console.log(err)
+//     }
+
+//     console.log("Registro deletado com sucesso!")
+// })
+
+// })
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const server = express()
 
+const db = require("./database/db")
+
 server.use(express.static("public"))
 
 const nunjucks = require("nunjucks")
@@ -14,11 +16,25 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+
+   req.query
+
+
    return res.render("create-point.html")
 })
 
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
- })
+
+   db.all(`SELECT * FROM places`, function(err, rows){
+    if(err) {
+        return console.log(err)
+    }
+    
+    const total = rows.length
+
+    return res.render("search-results.html", { places: rows, total:total})
+   })
+  
+})
 
 server.listen(3000)
